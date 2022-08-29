5b9e173bb407c5e07b722f6ae9f0d896fed02853
adamsickels00 <adamsickels00@gmail.com>
2022-08-29 17:52:05 -0400

    Added user authentication system with firebase

--- app/src/main/java/com/example/arkbabytracker/MainActivity.kt
+++ app/src/main/java/com/example/arkbabytracker/MainActivity.kt
@@ -9,7 +9,12 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.example.arkbabytracker.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,6 +22,42 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     private lateinit var navController: NavController
     lateinit var appBarConfiguration:AppBarConfiguration
+    private val mAuth = FirebaseAuth.getInstance()
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_create_intent]
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            initializeUI()
+
+        } else {
+            finish()
+        }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
@@ -25,21 +66,43 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(navController.currentDestination?.id == item.itemId)
             return true
+        if(item.itemId == R.id.logout_menu_item){
+            AuthUI.getInstance().signOut(this).addOnCompleteListener {
+                val intent = intent
+                finish()
+                startActivity(intent)
+            }
+        }
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(mAuth.currentUser == null) {
+            createSignInIntent()
+        } else{
+            initializeUI()
+        }
+
+    }
+
+    fun initializeUI(){
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         navController = (supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
 
 
         appBarConfiguration = AppBarConfiguration(setOf(R.id.babyTrackerGroupHolder,R.id.dino_stats_fragment,R.id.troughFragment,R.id.composeTestFragment,R.id.timerComposeFragment,R.id.userDefiedGroupsFragment,
-        R.id.colorSearchFragment),binding.drawerLayout)
+            R.id.colorSearchFragment),binding.drawerLayout)
+
         setSupportActionBar(binding.toolbar)
         binding.navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController,appBarConfiguration)
-
     }
+
 }
