5b28baa0db35b4ff68831e6f25ef6ca11c9136c1
adamsickels00 <adamsickels00@gmail.com>
2022-08-31 10:45:42 -0400

    Added tribe name input

--- app/src/main/java/com/example/arkbabytracker/tribes/TribeScreen.kt
+++ app/src/main/java/com/example/arkbabytracker/tribes/TribeScreen.kt
@@ -1,24 +1,22 @@
 package com.example.arkbabytracker.tribes
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.arkbabytracker.utils.MyTheme
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 @Composable
 fun TribeScreen(vm:TribeViewModel = viewModel()){
     val currentTribe = vm.getUserTribe()
     ShowTribeOrJoinOptions(currentTribe,
-        createTribeAction = {vm.createTribe("Test Name")},
+        createTribeAction = {s->vm.createTribe(s)},
         joinTribeAction = {},
         leaveTribeAction = {vm.leaveTribe(currentTribe)}
     )
@@ -27,11 +25,11 @@ fun TribeScreen(vm:TribeViewModel = viewModel()){
 @Composable
 fun ShowTribeOrJoinOptions(
     currentTribe: String?,
-    createTribeAction: () -> Unit,
+    createTribeAction: (String) -> Unit,
     joinTribeAction: () -> Unit,
     leaveTribeAction: () -> Unit
 ) {
-    Box(modifier=Modifier.fillMaxSize()) {
+    Box(modifier=Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         if (currentTribe == null) {
             ShowMakeOrJoinButtons(createTribeAction,joinTribeAction)
         } else {
@@ -51,16 +49,38 @@ fun ShowTribe(currentTribe: String, leaveTribeAction: () -> Unit) {
 }
 
 @Composable
-fun ShowMakeOrJoinButtons(createTribeAction:()->Unit,joinTribeAction: () -> Unit) {
-    Column {
+fun ShowMakeOrJoinButtons(createTribeAction:(String)->Unit,joinTribeAction: () -> Unit,showPopup:Boolean=false) {
+    var popupShowState by remember{ mutableStateOf(showPopup)}
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Not currently in a tribe", color = MaterialTheme.colors.onBackground)
-        Button(onClick = createTribeAction) {
+        Button(onClick = {popupShowState=true}) {
             Text("Create Tribe")
         }
         Button(onClick = joinTribeAction) {
             Text("Join Tribe")
         }
     }
+    if(popupShowState){
+        Popup(onDismissRequest = {popupShowState=false}) {
+            TribePopupContent(onSubmitButtonPressed = {s->popupShowState=false;createTribeAction(s)})
+
+        }
+    }
+}
+
+@Composable
+fun TribePopupContent(onSubmitButtonPressed: (String) -> Unit){
+        var text by remember{mutableStateOf("")}
+        Card(elevation = 8.dp, backgroundColor = MaterialTheme.colors.background) {
+            Column(Modifier.padding(64.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                TextField(value = text, onValueChange = {text=it}, label = {Text("Tribe Name")})
+                Spacer(modifier = Modifier.padding(10.dp))
+                Button(onClick = { onSubmitButtonPressed(text) }) {
+                    Text("Submit")
+                }
+            }
+        }
+
 }
 
 @Preview
@@ -71,4 +91,12 @@ fun PreviewTribeScreen(){
         ShowTribeOrJoinOptions(currentTribe = null, createTribeAction = {}, joinTribeAction = {}, leaveTribeAction = {})
     }
 
+}
+
+@Preview
+@Composable
+fun PopupPreview(){
+    MyTheme(isDarkTheme = true) {
+        TribePopupContent(onSubmitButtonPressed = {})
+    }
 }
